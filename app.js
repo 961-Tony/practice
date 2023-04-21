4fa528e948c99ce05bceee27975e805694cadfde
unknown <tonyayda7@gmail.com>
2023-04-20 21:41:16 -0700

    first

---
+++ app.js
@@ -0,0 +1,37 @@
+
+function show() {
+  var list = document.getElementsByClassName("list")[0];
+  if (list.style.display === "none") {
+    list.style.display = "block";
+  } else {
+    list.style.display = "none";
+  }
+}
+
+
+// Wait for the document to finish loading
+document.addEventListener('DOMContentLoaded', function() {
+
+  // Hide the spinner initially
+  document.querySelector('.load').style.display = 'none';
+
+  // Show the spinner while the page is loading
+  window.addEventListener('load', function() {
+    document.querySelector('.load').style.display = 'block';
+  });
+
+  // Hide the spinner once the page has finished loading
+  window.addEventListener('load', function() {
+    document.querySelector('.load').style.display = 'none';
+  });
+
+});
+
+function openEmail() {
+  var emailAddress = "tonyayda7@gmail.com";
+  window.location.href = "mailto:" + emailAddress;
+}
+
+
+
+
